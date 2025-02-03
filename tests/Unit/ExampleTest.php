1b3bdaf6cd044e3a5f3932f549f8a0678b8380ca
篠田 知志 <s_shinoda@MacBook-Pro.local>
2025-02-03 14:40:58 +0900

    静的解析ツールの導入

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
@@ -9,8 +11,9 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    public function testThatTrueIsTrue(): void
     {
+        // @phpstan-ignore method.alreadyNarrowedType
         $this->assertTrue(true);
     }
 }
